2be71198d6b35d485571b0e97f17117464ece3e1
CristianDDGA <crisalonso878@gmail.com>
2025-06-28 21:33:38 -0500

    Se aplica nuevas migraciones a la base de datos

---
+++ prisma/migrations/20250629012459_add_is_verified_to_cuenta/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `ENL_CER_PAR` on the `PARTICIPACIONES` table. All the data in the column will be lost.
+  - You are about to drop the column `ENL_CER_PAR_CUR` on the `PARTICIPACIONES_CURSO` table. All the data in the column will be lost.
+  - You are about to drop the column `OBSERVACIONES` on the `PARTICIPACIONES_CURSO` table. All the data in the column will be lost.
+  - You are about to drop the column `RESPONSABLE` on the `PARTICIPACIONES_CURSO` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "CUENTAS" ADD COLUMN     "emailVerificationExpiry" TIMESTAMP(3),
+ADD COLUMN     "emailVerificationToken" TEXT,
+ADD COLUMN     "isVerified" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "PARTICIPACIONES" DROP COLUMN "ENL_CER_PAR",
+ADD COLUMN     "CERTIFICADO_FILENAME" VARCHAR(255),
+ADD COLUMN     "CERTIFICADO_PDF" BYTEA,
+ADD COLUMN     "CERTIFICADO_SIZE" INTEGER;
+
+-- AlterTable
+ALTER TABLE "PARTICIPACIONES_CURSO" DROP COLUMN "ENL_CER_PAR_CUR",
+DROP COLUMN "OBSERVACIONES",
+DROP COLUMN "RESPONSABLE",
+ADD COLUMN     "CERTIFICADO_FILENAME" VARCHAR(255),
+ADD COLUMN     "CERTIFICADO_PDF" BYTEA,
+ADD COLUMN     "CERTIFICADO_SIZE" INTEGER;
